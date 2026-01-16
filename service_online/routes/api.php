983c42cc700ca5652930e8d4c00d913c51fd3fc2
maulana1903 <fatwaimam.fim@gmail.com>
2026-01-16 10:48:54 +0700

    update fix bug login hosting

--- service_online/routes/api.php
+++ service_online/routes/api.php
@@ -5,16 +5,15 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AdminApiController;
 use App\Http\Controllers\Api\ServiceRequestApiController;
 
-
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-//midleware admin untuk update
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/admins', [AdminApiController::class, 'index']);
     Route::post('/admins', [AdminApiController::class, 'store']);
     Route::put('/admins/{id}', [AdminApiController::class, 'update']);
     Route::delete('/admins/{id}', [AdminApiController::class, 'destroy']);
+    Route::get('/admins/{id}', [AdminApiController::class, 'show']);
 });
 
 Route::post('/service-requests', [ServiceRequestApiController::class, 'store']);
