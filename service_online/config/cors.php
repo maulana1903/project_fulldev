a522314e69d5f9d31ea28f214f062fb9895158ab
maulana1903 <fatwaimam.fim@gmail.com>
2026-01-16 10:53:22 +0700

    update allowed origin ke frontend hosting

--- service_online/config/cors.php
+++ service_online/config/cors.php
@@ -3,11 +3,10 @@
 return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    
+
     'allowed_headers' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000'],
-
+    'allowed_origins' => ['kudoikom.net'],
 
     'allowed_methods' => ['*'],
 
