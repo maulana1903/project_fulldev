9287693d2a6887df624a9ad24d59c7d9a3f7b51c
rayufacollection <favianrizqi3@gmail.com>
2026-01-20 19:00:12 +0700

    perbaikan minor

--- react/src/pages/AdminCreate.jsx
+++ react/src/pages/AdminCreate.jsx
@@ -1,52 +1,47 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "../api/axios";
-import Swal from "sweetalert2";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../api/axios';
+import Swal from 'sweetalert2';
 
 export default function AdminCreate() {
   const navigate = useNavigate();
 
-  // ambil user login
-  const authUser = JSON.parse(
-  localStorage.getItem("user") ||
-  sessionStorage.getItem("user") ||
-  "null"
-);
+  const authUser = JSON.parse(localStorage.getItem('user') || sessionStorage.getItem('user') || 'null');
 
   const [form, setForm] = useState({
-    name: "",
-    email: "",
-    password: "",
-    jabatan: "",
-    role_id: "2", // default ADMIN
+    name: '',
+    email: '',
+    password: '',
+    jabatan: '',
+    role_id: '2',
   });
 
   const submit = async (e) => {
     e.preventDefault();
     Swal.fire({
-    title: "Menyimpan...",
-    text: "Mohon tunggu sebentar",
-    allowOutsideClick: false,
-    didOpen: () => {
-      Swal.showLoading();
-    },
-  });
+      title: 'Menyimpan...',
+      text: 'Mohon tunggu sebentar',
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
     try {
-      await api.post("/admins", form);
+      await api.post('/admins', form);
       Swal.fire({
-        icon: "success",
-        title: "Berhasil",
-        text: "User berhasil Ditambahkan",
+        icon: 'success',
+        title: 'Berhasil',
+        text: 'User berhasil Ditambahkan',
         timer: 1500,
         showConfirmButton: false,
       });
-      navigate("/superadmin");
+      navigate('/superadmin');
     } catch (err) {
-      const pesan_eror =err.response?.data?.message || "Gagal tambah user";
+      const pesan_eror = err.response?.data?.message || 'Gagal tambah user';
       Swal.fire({
-        icon: "error",
-        title: "Gagal",
+        icon: 'error',
+        title: 'Gagal',
         text: pesan_eror,
       });
     }
@@ -64,73 +59,31 @@ export default function AdminCreate() {
             {/* NAMA */}
             <div className="mb-3">
               <label className="form-label">Nama</label>
-              <input
-                className="form-control"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, name: e.target.value })
-                }
-              />
+              <input className="form-control" required onChange={(e) => setForm({ ...form, name: e.target.value })} />
             </div>
             <div className="mb-3">
               <label className="form-label">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, email: e.target.value })
-                }
-              />
+              <input type="email" className="form-control" required onChange={(e) => setForm({ ...form, email: e.target.value })} />
             </div>
             <div className="mb-3">
               <label className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                required
-                onChange={(e) =>
-                  setForm({ ...form, password: e.target.value })
-                }
-              />
+              <input type="password" className="form-control" required onChange={(e) => setForm({ ...form, password: e.target.value })} />
             </div>
             <div className="mb-3">
               <label className="form-label">Jabatan</label>
-              <input
-                type="text"
-                className="form-control"
-                required
-                value={form.jabatan}
-                onChange={(e) =>
-                  setForm({ ...form, jabatan: e.target.value })
-                }
-              />
+              <input type="text" className="form-control" required value={form.jabatan} onChange={(e) => setForm({ ...form, jabatan: e.target.value })} />
             </div>
             <div className="mb-3">
               <label className="form-label">Role</label>
-              <select
-                className="form-control"
-                value={form.role_id}
-                onChange={(e) =>
-                  setForm({ ...form, role_id: e.target.value })
-                }
-              >
-                {authUser?.is_root && (
-                  <option value="1">Super Admin</option>
-                )}
+              <select className="form-control" value={form.role_id} onChange={(e) => setForm({ ...form, role_id: e.target.value })}>
+                {authUser?.is_root && <option value="1">Super Admin</option>}
 
                 <option value="2">Admin</option>
               </select>
             </div>
             <div className="d-flex gap-2">
-              <button className="btn btn-primary">
-                Simpan
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => navigate(-1)}
-              >
+              <button className="btn btn-primary">Simpan</button>
+              <button type="button" className="btn btn-secondary" onClick={() => navigate(-1)}>
                 Batal
               </button>
             </div>
